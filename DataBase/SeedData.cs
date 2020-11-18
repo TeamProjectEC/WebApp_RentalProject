8edc8679d0dd44bf2152be3c5788105d0a917ea9
Mousa Azimi <mousa.azimi@utb.ecutbildning.se>
2020-11-18 16:19:40 +0100

    Add project files.

---
+++ DataBase/SeedData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataBase
+{
+    class SeedData
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
