424730737a22c7cc72c851f4a90b6bcaff9c5c2b
Mousa Azimi <mousa.azimi@utb.ecutbildning.se>
2020-12-04 20:37:44 +0100

    placera bilder welcome

--- WebApp/Welcome.xaml.cs
+++ WebApp/Welcome.xaml.cs
@@ -1,5 +1,4 @@
-﻿using DataBase;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -10,6 +9,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using DataBase;
 
 namespace WebApp
 {
@@ -22,9 +22,90 @@ namespace WebApp
         {
             InitializeComponent();
 
-            //State.Movie = API.GetMovieSlice(0, 100);
+            
+            int movie_skip_count = 0;
+            int movie_take_count = 100;
+            State.Movie = API.GetMovieSlice(movie_skip_count, movie_take_count);
 
+            int column_count = MovieGrid.ColumnDefinitions.Count;
+
+          
+            int row_count = (int)Math.Ceiling((double)State.Movie.Count / (double)column_count);
+
+            for (int y = 0; y < row_count; y++)
+            {
+                
+                MovieGrid.RowDefinitions.Add(
+                    new RowDefinition()
+                    {
+                        Height = new GridLength(350, GridUnitType.Pixel)
+                    });
+
+               
+                for (int x = 0; x < column_count; x++)
+                {
+                    
+                    int i = y * column_count + x;
+                   
+                    if (i < State.Movie.Count)
+                    {
+                       
+                        var movie = State.Movie[i];
+                      
+                        var image = new Image()
+                        {
+                            Cursor = Cursors.Hand, 
+                            HorizontalAlignment = HorizontalAlignment.Center,
+                            VerticalAlignment = VerticalAlignment.Center,
+                            Margin = new Thickness(10, 10, 10, 10),
+                        };
+                        image.MouseUp += Image_MouseUp;
+
+                        try
+                        {
+                            image.Source = new BitmapImage(new Uri(movie.ImageURL)); 
+                        }
+                        catch (Exception e) when
+                            (e is ArgumentNullException ||
+                             e is System.IO.FileNotFoundException ||
+                             e is UriFormatException)
+                        {
+                            
+                            image.Source = new BitmapImage(new Uri("https://wolper.com.au/wp-content/uploads/2017/10/image-placeholder.jpg"));
+                        }
+
+                       
+                        MovieGrid.Children.Add(image);
+                        
+                        Grid.SetRow(image, y);
+                        Grid.SetColumn(image, x);
+                    }
+                }
+            }
         }
+
+        
+        private void Image_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            
+            var x = Grid.GetColumn(sender as UIElement);
+            var y = Grid.GetRow(sender as UIElement);
+
+           
+            int i = y * MovieGrid.ColumnDefinitions.Count + x;
+            
+            State.Pick = State.Movie[i];
+
+            
+            if (API.RegisterSale(State.User, State.Pick))
+                
+                MessageBox.Show("All is well and you can download your movie now.", "Sale Succeeded!", MessageBoxButton.OK, MessageBoxImage.Information);
+            else
+                MessageBox.Show("An error happened while buying the movie, please try again at a later time.", "Sale Failed!", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+        }
+    
+
+
 
         private void Button_Click_Home(object sender, RoutedEventArgs e)
         {
